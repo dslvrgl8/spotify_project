c7b519a1b54925136ed57b854495d77e6dc5ae4b
Dylan Silverglate <dylansilverglate@Dylans-Air.lan>
2023-06-29 17:11:18 -0700

    spotify project

---
+++ main_app/views.py
@@ -0,0 +1,100 @@
+from django.shortcuts import redirect
+from django.views import View
+
+from .models import Artist, Song, Playlist
+# This will import the class we are extending 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.base import TemplateView
+from django.views.generic import DetailView
+
+# Create your views here.
+# I created a class named Home that's a child of TemplateView and is inheriting what's built into the parent class
+class Home(TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["playlists"] = Playlist.objects.all()
+        return context
+
+class About(TemplateView):
+    template_name = "about.html"
+
+class SongList(TemplateView):
+    template_name = "song_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["songs"] = Song.objects.all()
+        return context
+
+# class Song:
+#     def __init__(self, title, album):
+#         self.title = title
+#         self.album = album
+
+# songs = [
+#     Song("Lost", "stressed & depressed by The Circle Sessions"),
+#     Song("Never Ending Song", "Never Ending Song")
+# ]
+
+class ArtistCreate(CreateView):
+    model = Artist
+    fields = ['name', 'img', 'bio', 'verified_artist']
+    template_name = "artist_create.html"
+    success_url = '/artists/'
+
+class ArtistList(TemplateView):
+    template_name = "artist_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # This is essentially a query built into Django that's saying SELECT * FROM main_app_artist
+        # self.request.GET is a dictionary just like in express we have req.query
+        # print(self.request.GET)
+        name = self.request.GET.get('name')
+        if name != None:
+            # This is essentially a regex matcher that's built in for us. It's searching for anything where the name of the artist contains (at any point) this name if there is a query
+            context["artists"] = Artist.objects.filter(name__icontains=name)
+        else:
+            context["artists"] = Artist.objects.all()
+        return context
+    
+class ArtistDetail(DetailView):
+    model = Artist
+    template_name = "artist_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["playlists"] = Playlist.objects.all()
+        return context
+
+class ArtistUpdate(UpdateView):
+    model = Artist
+    fields = ['name', 'img', 'bio', 'verified_artist']
+    template_name = "artist_update.html"
+    success_url = '/artists/'
+
+class ArtistDelete(DeleteView):
+    model = Artist
+    template_name = "artist_delete_confirmation.html"
+    success_url = '/artists/'
+
+class SongCreate(View):
+
+    def post(self, request, pk):
+        title = request.POST.get("title")
+        length = request.POST.get("length")
+        artist = Artist.objects.get(pk=pk)
+        Song.objects.create(title=title, length=length, artist=artist)
+        return redirect('artist_detail', pk=pk)
+    
+class PlaylistSongAssoc(View):
+
+    def get(self, request, pk, song_pk):
+        assoc = request.GET.get('assoc')
+        if assoc == "remove":
+            Playlist.objects.get(pk=pk).songs.remove(song_pk)
+        if assoc == "add":
+            Playlist.objects.get(pk=pk).songs.add(song_pk)
+        return redirect('home')
